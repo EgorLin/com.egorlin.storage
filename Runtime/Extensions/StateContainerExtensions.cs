a8d6a76bcd96f399e39d9332f7070e6e33bb59b4
Egor <egor.priemko@protonmail.com>
2026-05-21 14:40:56 +0300

    add new extensions

--- Runtime/Extensions/StateContainerExtensions.cs
+++ Runtime/Extensions/StateContainerExtensions.cs
@@ -6,12 +6,37 @@ namespace EgorLin.Storage.Extensions
 {
     public static class StateContainerExtensions
     {
-        public static DiContainerMigration<TState> BindStateWithMigration<TState>(this DiContainer container) where TState : struct, IState 
+        public static DiContainer BindStateRW<TState>(this DiContainer container) where TState : struct, IState 
+        {
+            container.BindSingleton(typeof(StateRW<TState>), typeof(StateRW<TState>));
+            
+            return container;
+        }
+        
+        public static DiContainerMigration<TState> BindStateROWithMigration<TState>(this DiContainer container) where TState : struct, IState 
         {
             container.BindSingleton(typeof(StateHandle<TState>), typeof(StateHandle<TState>), typeof(IStateHandle));
 
             container.BindSingleton(typeof(StateRO<TState>), typeof(StateRO<TState>));
+
+            return new DiContainerMigration<TState>(container);
+        }
+        
+        public static void BindStateROWithoutMigration<TState>(this DiContainer container) where TState : struct, IState 
+        {
+            container.BindSingleton(typeof(StateHandle<TState>), typeof(StateHandle<TState>), typeof(IStateHandle));
+
+            container.BindSingleton(typeof(StateRO<TState>), typeof(StateRO<TState>));
+            
+            container.BindSingleton(typeof(MigrationDummy<TState>), typeof(Migration<TState>));
+        }
+        
+        public static DiContainerMigration<TState> BindStateWithMigration<TState>(this DiContainer container) where TState : struct, IState 
+        {
+            container.BindSingleton(typeof(StateHandle<TState>), typeof(StateHandle<TState>), typeof(IStateHandle));
+
             container.BindSingleton(typeof(StateRW<TState>), typeof(StateRW<TState>));
+            container.BindSingleton(typeof(StateRO<TState>), typeof(StateRO<TState>));
 
             return new DiContainerMigration<TState>(container);
         }
@@ -20,8 +45,8 @@ namespace EgorLin.Storage.Extensions
         {
             container.BindSingleton(typeof(StateHandle<TState>), typeof(StateHandle<TState>), typeof(IStateHandle));
 
-            container.BindSingleton(typeof(StateRO<TState>), typeof(StateRO<TState>));
             container.BindSingleton(typeof(StateRW<TState>), typeof(StateRW<TState>));
+            container.BindSingleton(typeof(StateRO<TState>), typeof(StateRO<TState>));
             
             container.BindSingleton(typeof(MigrationDummy<TState>), typeof(Migration<TState>));
         }
