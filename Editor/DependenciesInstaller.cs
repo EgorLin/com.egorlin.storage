c1827816782ec64d61b46643b406a299c6259e62
Egor <egor.priemko@protonmail.com>
2026-05-22 14:14:47 +0300

    update installer

--- Editor/DependenciesInstaller.cs
+++ Editor/DependenciesInstaller.cs
@@ -16,41 +16,58 @@ namespace EgorLin.Storage.Editor
         {
             var listRequest = Client.List(offlineMode: false, includeIndirectDependencies: true);
             EditorApplication.update += WaitForList;
-            
-            return;
 
+            return;
             void WaitForList()
             {
                 if (!listRequest.IsCompleted)
                 {
                     return;
                 }
-                
+
                 EditorApplication.update -= WaitForList;
 
-                var installed = new HashSet<string>();
+                var installedVersions = new Dictionary<string, System.Version>();
                 if (listRequest.Result != null)
                 {
                     foreach (var p in listRequest.Result)
                     {
-                        installed.Add(p.name);
+                        if (System.Version.TryParse(p.version, out var v))
+                            installedVersions[p.name] = v;
                     }
                 }
 
                 var toInstall = new List<string>();
                 foreach (var (name, url) in Packages)
                 {
-                    if (!installed.Contains(name))
+                    var desiredVersion = ParseVersionFromUrl(url);
+
+                    if (!installedVersions.TryGetValue(name, out var installedVersion))
+                    {
+                        toInstall.Add(url);
+                    }
+                    else if (desiredVersion != null && desiredVersion > installedVersion)
                     {
                         toInstall.Add(url);
                     }
                 }
 
                 if (toInstall.Count > 0)
-                {
                     Client.AddAndRemove(toInstall.ToArray());
-                }
             }
+        }
+
+        private static System.Version ParseVersionFromUrl(string url)
+        {
+            var hash = url.LastIndexOf('#');
+            
+            if (hash < 0)
+            {
+                return null;
+            }
+            
+            var tag = url.Substring(hash + 1);
+            return System.Version.TryParse(tag, out var v) ? v : null;
         }
     }
 }
